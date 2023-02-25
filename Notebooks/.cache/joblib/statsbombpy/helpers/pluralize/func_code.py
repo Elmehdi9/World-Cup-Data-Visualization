a2f2174288af7ebfe5e7ee1e4049e7a8159e66de
Elmehdi_Dahbi <dahbielmehdi05@gmail.com>
2023-02-25 01:30:39 +0100

    Cache deletion

--- Notebooks/.cache/joblib/statsbombpy/helpers/pluralize/func_code.py
+++
@@ -1,9 +0,0 @@
-# first line: 85
-@memory.cache
-def pluralize(word):
-    word = engine.plural(word)
-    word = word.replace("*", "")
-    word = word.replace("-", "_")
-    word = word.replace(" ", "_")
-    word = word.lower()
-    return word
